20a826c23caf0972d8083c0e8856c994bc0a3a60
XxAGMGxX <85764970+XxAGMGxX@users.noreply.github.com>
2021-06-20 21:23:42 -0500

    Add files via upload

---
+++ 15-06.py
@@ -0,0 +1,22 @@
+class For:
+    def __init__(self,lim=6):
+        self.n=lim
+
+    def usoFor(self):
+        nombre = "Andy"
+        datos=["Andy",19,True]
+        numeros=(2,5,6,4,1)
+        estudiante = {"nombre": "Andy", "edad": 19, "fac": "faci"}
+        listaNotas = [(40,50),(30,40,50),(60,50)]
+        listaAlumnos = [{"nombre": "Andy","final":70},{"nombre":"Jose","final"80:},{"nombre": "Jimmy","final"90:}]
+
+
+        for pos,elem in enumerate(nombre):
+            if pos%2==0 and pos!=0
+                print(elem)
+
+bucle1 = For()
+bucle1.usoFor()
+
+    #bucle2 = For(12)
+    #bucle2.usoFor()
